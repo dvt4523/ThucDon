5789fe3330f1b6b0da4bfc8d413d06fb11fe3b0a
Leo Tr <truongvietduc@gmail.com>
2019-12-07 04:15:49 +0700

    [BE]Add services files & Food Data page

---
+++ ThucDon/Services/RawFoodServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThucDon.Data;
+using ThucDon.Models;
+
+namespace ThucDon.Services
+{
+    public class RawFoodServices
+    {
+        private readonly ApplicationDbContext _db;
+        public RawFoodServices (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
